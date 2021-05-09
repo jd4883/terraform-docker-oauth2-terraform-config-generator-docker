e71b65a7988a6467a6c88d95ccd8d68cf20c2bc2
Jacob Dresdale <15719920+jd4883@users.noreply.github.com>
2021-05-08 19:22:55 -0700

    some syntax cleanup for tf15

--- resources.tf
+++ resources.tf
@@ -7,9 +7,7 @@ resource "null_resource" "authenticated-emails" {
   }
 }
 
-resource "docker_image" "image" {
-  name = "quay.io/oauth2-proxy/oauth2-proxy:latest"
-}
+resource "docker_image" "image" { name = "quay.io/oauth2-proxy/oauth2-proxy:latest" }
 
 resource "docker_container" "oauth2" {
   name    = local.name
@@ -20,7 +18,16 @@ resource "docker_container" "oauth2" {
     ignore_changes  = [user]
   }
   dynamic "labels" {
-    for_each = var.labels
+    for_each = merge(
+            var.labels,
+            {
+              "traefik.http.routers.${lower(local.name)}.rule" : "Host(${join(",", formatlist("`%s`", [for i in tolist(try(var.cnames, [var.name])) : join(".", [i, var.domain])]))})",
+              "traefik.http.routers.${lower(local.name)}.service" : local.name,
+              "traefik.http.services.${lower(local.name)}.loadbalancer.server.port" : var.okta.oauth_port,
+              "traefik.http.middlewares.${lower(local.name)}.headers.sslhost" : join(",", formatlist("`%s`", [for i in tolist(try(var.cnames, [var.name])) : join(".", [i, var.domain])])),
+              "traefik.http.middlewares.${lower(local.name)}-compression.compress" : false
+            }
+    )
     content {
       label = replace(labels.key, "PLACEHOLDER_KEY", local.name)
       value = tobool(
@@ -33,29 +40,16 @@ resource "docker_container" "oauth2" {
       ) : labels.value
     }
   }
-  dynamic "labels" {
-    for_each = {
-      "traefik.http.routers.${lower(local.name)}.rule" : "Host(${join(",", formatlist("`%s`", [for i in tolist(try(var.cnames, [var.name])) : join(".", [i, var.domain])]))})",
-      "traefik.http.routers.${lower(local.name)}.service" : local.name,
-      "traefik.http.services.${lower(local.name)}.loadbalancer.server.port" : var.okta.oauth_port,
-      "traefik.http.middlewares.${lower(local.name)}.headers.sslhost" : join(",", formatlist("`%s`", [for i in tolist(try(var.cnames, [var.name])) : join(".", [i, var.domain])])),
-      "traefik.http.middlewares.${lower(local.name)}-compression.compress" : false
-    }
-    content {
-      label = labels.key
-      value = labels.value
-    }
-  }
   volumes {
     read_only      = true
     host_path      = abspath("${path.module}/rendered/authenticated-emails.txt")
     container_path = "/authenticated-emails.txt"
   }
-  networks_advanced {
-    name = data.docker_network.backend.name
-  }
-  networks_advanced {
-    name = data.docker_network.frontend.name
+  dynamic "networks_advanced" {
+    for_each = toset([data.docker_network.backend.name, data.docker_network.frontend.name])
+    content {
+      name = data.docker_network.backend.name
+    }
   }
   depends_on = [null_resource.authenticated-emails]
 }
